13bf74d695a3ef34ad481715eb5ebb03a750dd15
Sydney <k2002418@kcl.ac.uk>
2022-10-13 10:56:20 +0100

    main

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,14 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    #items is a list
+    for i in range(len(items)):
+        count = 0
+        items[i] = str(items[i])
+
+        for j in range(len(items)):  
+            if items[i] == items [j]:
+                count += 1 
+            frequencies[items[i]] = count   
+              
     return frequencies
